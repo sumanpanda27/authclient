09636f0fb91368f356f725876e6fefd2101b4432
Suman Panda <sumanpanda27@gmail.com>
2025-03-08 11:23:23 +0530

    fixed complete

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { toast } from "react-toastify";
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const { userData, backendUrl, setUserData, setIsLoggedin } =
+  const { userData, backendUrl, setUserData, setIsLoggedin, isLoggedin } =
     useContext(AppContent);
 
   const sendVerificationOtp = async () => {
@@ -39,8 +39,6 @@ const Navbar = () => {
       if (setIsLoggedin === false) {
         navigate("/");
         toast.success({ message: "Logout Successfully" });
-      } else {
-        toast.error(data.message);
       }
     } catch (error) {
       toast.error(error.message);
@@ -50,7 +48,7 @@ const Navbar = () => {
   return (
     <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
       <img src={assets.logo} alt="" className="w-28 sm:w-32" />
-      {userData ? (
+      {userData && isLoggedin ? (
         <div className="w-8 h-8 flex justify-center items-center rounded-full bg-black text-white  relative group cursor-pointer">
           {userData.name[0].toUpperCase()}
           <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10">
